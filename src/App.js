54996f07f7787377c47933abe0ab80d91a9865b7
Jobeda Nur <jobedanur265@gmail.com>
2021-06-02 02:33:47 +0600

    header & drawer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
 import HeaderMain from './components/HeaderMain/HeaderMain';
-import SideBar from './components/SideBar/SideBar';
 import { createContext, useState } from 'react';
 
 export const UserContext = createContext();
@@ -14,7 +13,6 @@ function App() {
     <UserContext.Provider value={[drawer, setDrawer]}>
     <div className="App">
       <Header></Header>
-      {/* <SideBar></SideBar> */}
       <HeaderMain></HeaderMain>
     </div>
     </UserContext.Provider >
